617591a72f35f3a799206b4921d3de938f579a35
SalehZo-Niloy <msnniloy7@gmail.com>
2022-12-12 04:59:47 +0600

    image bug fix try 1

--- pages/projects/project1.js
+++ pages/projects/project1.js
@@ -87,9 +87,9 @@ const project1 = () => {
                 <div className='flex flex-col gap-8'>
                     <h3 className='text-xl font-semibold text-primary'>Site's Screenshots:</h3>
 
-                    <img src="/project1-images/img1.png" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
-                    <img src="/project1-images/img2.png" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
-                    <img src="/project1-images/img3.png" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
+                    <img src="/project1-images/img1.PNG" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
+                    <img src="/project1-images/img2.PNG" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
+                    <img src="/project1-images/img3.PNG" alt="" className='w-full rounded-lg shadow-primary shadow-lg' />
 
                 </div>
             </div>
